72ea076ba54a12f0c6856be1a284aa6fc6d86c4a
Aaron Liu <a3liu@ucsd.edu>
2018-04-20 01:18:07 -0700

    Temporarily changed the arrow syntax to function since my version of node is apparently too old to compile it

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(express.static('static_files'));
 
 //Routes for actual pages
-app.get('/', /* Function for displaying homepage */);
+/*app.get('/', /* Function for displaying homepage);*/
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000'); 
+app.listen(3000, function() {
+    console.log('Server listening on port 3000');
 });
