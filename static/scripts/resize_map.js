3a251d0bac3e808c647beda4e8ef30dd67a584ba
Lam <lamqpham101@gmail.com>
2018-04-24 02:44:55 -0700

    fixed leaflet

--- static/scripts/resize_map.js
+++ static/scripts/resize_map.js
@@ -1,8 +1,6 @@
-
-// Replace this.mapmargin tag with ID DIV tag for the main container viewport
 var resize_map = {};
 resize_map.mapmargin = 0;
-resize_map.minHeight = 415;
+resize_map.minHeight = 400;
 resize_map.getHeight = function() {
     this.mapmargin = $('.container-fluid').height();
 };
@@ -10,15 +8,14 @@ resize_map.getHeight = function() {
 resize_map.getHeight();
 $(window).bind("resize", resize);
 resize();
+//document.write($('.container-fluid').height());
+
 
 function resize(){
     if($(window).width() >= resize_map.minHeight){
-        $('#map').css("height", ($(window).height() - resize_map.mapmargin));
-        // setTimeout, to Handle using the Maximize button which needs more time to respond correctly
-        setTimeout(function() {
-            $('#map').css("height", ($(window).height() - resize_map.mapmargin));
-          }, 150);
-    // Set the map size cannot be smaller than the width on mobile devices.
+        $('#map').css("height", ($(window).height() - resize_map.mapmargin + 100));
+        //document.write($(window).height() + " " + resize_map.mapmargin);
+    // Map size cannot be smaller than the width on mobile devices.
     }else{
         if($(window).height() <= resize_map.minHeight){
             $('#map').css("height", resize_map.minHeight);
