ab8b5b0e37527a962391ac7a6f46b998745d981e
Grace Chen <gpc001@ucsd.edu>
2018-04-29 13:22:10 -0700

    clicks

--- server.js
+++ server.js
@@ -6,85 +6,108 @@ const app = express(); //instantiate express object
 app.use(express.static(path.join(__dirname, 'static')));
 
 const tempDatabase = {
-  'Redwood': {
-    intro: 'Redwood National and State Parks are a string of protected forests, ' +
-    'beaches and grasslands along Northern California’s coast. Jedediah Smith ' +
-    'Redwoods State Park has trails through dense old-growth woods. Prairie Creek' +
-    ' Redwoods State Park is home to Fern Canyon, with its high, plant-covered walls. ' +
-    'Roosevelt elk frequent nearby Elk Prairie. Giant redwood clusters include ' + 
-    'Redwood National Park’s Lady Bird Johnson Grove.',
-    trails: [
-    '<option>Damnation</option>', 
-    '<option>Redwood Creek</option>',
-    '<option>Stout Grove</option>'
-    ],
-    pic: '<img src="redwood.jpeg" width="50%">'
-  },
-  'Sequoia': {
-    intro: '',
-    trails: '<option>Hazelwood Nature Trail</option>', 
-    pic: '<img src="sequoia.jpeg" width="50%">'
-  },
-  'Joshua Tree': {
-    intro: '',
-    trails: '<option>Arch Rock</option>', 
-    pic: [
-    '<img src="jtree.jpeg" width="50%">',
-    '<img src="joshua1.jpg" width="50%">'
-    ]
-  },
-  'Alcatraz Island': {
-    intro: '',
-    trails: '<option>Agave Trail</option>',
-    pic: '<img src="alcatraz.jpg" width="50%">'
-  },
-  'Yosemite': {
-    intro: '',
-    trails: [
-    '<option>Half Dome</option>',
-    '<option>Panorama</option>'
-    ],
-    pic: '<img src="yosemite.jpg" width="50%">'
-  },
-  'Death Valley': {
-    intro: '',
-    trails: [
-    '<option>Badwater Basin</option>',
-    '<option>Gower Gulch</option>',
-    '<option>Golden Canyon</option>'
-    ],
-    pic: '<img src="deathvalley.jpg" width="50%">'
-  }
+	'Redwood': {
+		intro: 'Redwood National and State Parks are a string of protected forests, ' +
+		'beaches and grasslands along Northern California’s coast. Jedediah Smith ' +
+		'Redwoods State Park has trails through dense old-growth woods. Prairie Creek' +
+		' Redwoods State Park is home to Fern Canyon, with its high, plant-covered walls. ' +
+		'Roosevelt elk frequent nearby Elk Prairie. Giant redwood clusters include ' + 
+		'Redwood National Park’s Lady Bird Johnson Grove.',
+		trails: [
+		{
+			name: 'Damnation',
+			length: '0.5 miles',
+			difficulty: 'Easy'
+		},
+		{
+			name: 'Redwood Creek',
+			length: '1.5 miles',
+			difficulty: 'Medium'
+		},
+		{
+			name: 'Stout Grove',
+			length: '2.5 miles',
+			difficulty: 'Hard'
+		}
+		// name: {
+		// 	'<option>Damnation</option>', 
+		// 	'<option>Redwood Creek</option>',
+		// 	'<option>Stout Grove</option>'
+		// },
+		],
+		pic: '<img src="redwood.jpeg" width="50%">'
+	},
+	'Sequoia': {
+		intro: '',
+		trails: [
+			{
+				name: 'Hazelwood Nature Trail',
+				length: '2 miles',
+				difficulty: 'Medium'
+			}
+		],
+		pic: '<img src="sequoia.jpeg" width="50%">'
+	},
+	'Joshua Tree': {
+		intro: '',
+		trails: '<option>Arch Rock</option>', 
+		pic: [
+		'<img src="jtree.jpeg" width="50%">',
+		'<img src="joshua1.jpg" width="50%">'
+		]
+	},
+	'Alcatraz Island': {
+		intro: '',
+		trails: '<option>Agave Trail</option>',
+		pic: '<img src="alcatraz.jpg" width="50%">'
+	},
+	'Yosemite': {
+		intro: '',
+		trails: [
+		'<option>Half Dome</option>',
+		'<option>Panorama</option>'
+		],
+		pic: '<img src="yosemite.jpg" width="50%">'
+	},
+	'Death Valley': {
+		intro: '',
+		trails: [
+		'<option>Badwater Basin</option>',
+		'<option>Gower Gulch</option>',
+		'<option>Golden Canyon</option>'
+		],
+		pic: '<img src="deathvalley.jpg" width="50%">'
+	}
 };
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/static/home-page.html');
+	res.sendFile(__dirname + '/static/home-page.html');
 });
 
 app.get('/map-page', (req, res) => {
-    res.sendFile(__dirname + '/static/index2.html');
+	res.sendFile(__dirname + '/static/index2.html');
 });
 
 app.get('/parks', (req, res) => {
-  const allParks = Object.keys(tempDatabase);
-  console.log('allParks is: ', allParks);
-  res.send(allParks);
+	const allParks = Object.keys(tempDatabase);
+	console.log('allParks is: ', allParks);
+	res.send(allParks);
 });
 
 app.get('/parks/:parkid', (req, res) => {
-  const parkToLookUp = req.params.parkid;
-  const val = tempDatabase[parkToLookUp];
+	const parkToLookUp = req.params.parkid;
+	const val = tempDatabase[parkToLookUp];
 
-  console.log(parkToLookUp, '->', val);
-  if (val) {
-    res.send(val);
-  }
-  else {
-    res.send({});
-  }
+	console.log(parkToLookUp, '->', val);
+	if (val) {
+		res.send(val);
+	}
+	else {
+		res.send({});
+	}
 
 });
 
 app.listen(3000, () => {
-  console.log('Listening on Port 3000');
+	console.log('Listening on Port 3000');
 });
--- static/pointPoppy.js
+++ static/pointPoppy.js
@@ -25,7 +25,7 @@ $j(document).ready(() => {
     // get park info
     var idText = $('#' + event.target.id).text();
     const reqURL = 'parks/' + idText;
-  
+
     $j.ajax({
       url: reqURL,
       type: 'GET',
@@ -34,7 +34,18 @@ $j(document).ready(() => {
         console.log(data.pic);
         console.log(data.trails);
         $('#intro').html(data.intro);
-        $("#trailSelect").html(data.trails);
+        
+        // load trail names into select box
+        if ($("#trailSelect").html() == 0 || $("#trailSelect").html() != data.trails) {
+         $("#trailSelect").html(''); // clear select box
+         for (var i = 0; i < data.trails.length; i++) {
+          // append correct trail names
+          let trail_option = '<option value="' + i + '">' + data.trails[i].name + '</option>';
+          $("#trailSelect").append(trail_option);
+        }
+      }
+
+
         // $("#parkPic").attr("src", data.pic);
         $('#pics').html(data.pic);
       }
@@ -50,13 +61,19 @@ $j(document).ready(() => {
 
 });
 
+// trail/gallery tab functions
 function getParkData(trailgallery) {
-    var i;
-    var x = document.getElementsByClassName("parkinfo");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none"; 
-    }
-    document.getElementById(trailgallery).style.display = "block"; 
+  var i;
+  var x = document.getElementsByClassName("parkinfo");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  document.getElementById(trailgallery).style.display = "block"; 
+};
+
+// select trail box - insert length and difficulty into box
+function showTrailInfo(trail_name) {
+  alert(trail_name);
 
 };
 
