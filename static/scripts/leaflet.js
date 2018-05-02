678d6960e4c21595543f2de2c6f18cf4cd777dc6
Grace Chen <gpc001@ucsd.edu>
2018-05-02 16:43:10 -0700

    narrow down map

--- static/scripts/leaflet.js
+++ static/scripts/leaflet.js
@@ -5,33 +5,175 @@ let park, popular;
 
 // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3BjMDAxIiwiYSI6ImNqZ2JtYWphNzBnNnczMmx6bXNkeGFhYzkifQ.6dwOvoXOP5Oln1ltOiI6Bw', {
-     maxZoom: 18,
-     id: 'mapbox.streets',
-     detectRetina: true,
-     accessToken: 'pk.eyJ1IjoiY2xvdWRtYW4iLCJhIjoiY2o0amdnOGhkMDlnMDJ2bWh0eGRrazMxMiJ9.fAHOsFqx2ZK0t07TAy8ckA'
- }).addTo(my_map);
+       maxZoom: 18,
+       id: 'mapbox.streets',
+       detectRetina: true,
+       accessToken: 'pk.eyJ1IjoiY2xvdWRtYW4iLCJhIjoiY2o0amdnOGhkMDlnMDJ2bWh0eGRrazMxMiJ9.fAHOsFqx2ZK0t07TAy8ckA'
+   }).addTo(my_map);
+
+// popup functions
+// populate map with circles
+$j.ajax({
+    url: NPSurl,
+    method: 'GET',
+}).done((result) => {
+      getCoords(result); // create map circles
+  }).fail((err) => {
+    throw err;
+});
+
+// display park info on the right side
+function dataCall(e) {
+    $j.ajax({
+        url: NPSurl,
+        method: 'GET',
+    }).done((result) => {
+        displayParkInfo(e.target.id, result);
+    }).fail((err) => {
+        throw err;
+    });
+
+    // show trail select box
+    if (document.getElementById("Gallery").style.display === "none") {
+        document.getElementById("Trails").style.display = "block";
+        document.getElementById("Weather").style.display = "none";
+    } else {
+        document.getElementById("Trails").style.display = "none";
+        document.getElementById("Weather").style.display = "none";
+    }
+
+	// get park info from map
+    var idText = $('#' + e.target.id).text();
+    const reqURL = 'parks/' + idText;
+
+    console.log(reqURL);
+
+    $j.ajax({
+        url: reqURL,
+        type: 'GET',
+        dataType: 'json',
+        success: (data) => {
+
+            $('#intro').html(data.intro);
+
+            // load trail names into select box
+            if ($("#trailSelect").html() == 0 || $("#trailSelect").html() != data.trails) {
+                $("#trailSelect").html(''); // clear select box
+
+                for (var i = 0; i < data.trails.length; i++) {
+                    // append correct trail names
+                    let trail_option = '<option value="' + i + '">' + data.trails[i].name + '</option>';
+                    $("#trailSelect").append(trail_option);
+                }
+                trail_data = data.trails;
+            }
+        }
+    });
+}
+
+function circleClick(e) {
+    my_map.fitBounds(e.target.getBounds());
+}
+
+// get coordinates of park and draw a circle
+function getCoords(parkInfo) {
+    for (let j = 0; j < parkInfo.data.length; j++) {
+        for (let k = 0; k < parkCodes.length; k++) {
+            // only put circle on map if it is one we want to have
+            if (parkInfo.data[j].parkCode != parkCodes[k]) {
+                continue;
+            } else {
+
+// get lat and longitude coordinates
+let lat = '';
+let long = '';
+let i = 0;
+
+if (parkInfo.data[j].latLong === "") { // validity check
+    continue;
+}
+
+// parse out lat and long coordinates
+while (parkInfo.data[j].latLong[i] != ':') {
+    i++;
+}
+
+while (parkInfo.data[j].latLong[i + 1] != ',') {
+    lat = lat + parkInfo.data[j].latLong[i + 1];
+    i++;
+}
+
+while (parkInfo.data[j].latLong[i] != ':') {
+    i++;
+}
+
+for (i; i < parkInfo.data[j].latLong.length - 1; i++) {
+    long = long + parkInfo.data[j].latLong[i + 1];
+}
+
+// check if park is popular
+if (parkInfo.data[j].parkCode === 'redw' || 
+    parkInfo.data[j].parkCode === 'seki' ||
+    parkInfo.data[j].parkCode === 'jotr' ||
+    parkInfo.data[j].parkCode === 'deva' ||
+    parkInfo.data[j].parkCode === 'yose') {
+    popular = 1;
+} else {
+  popular = 0;
+}
+
+// create clickable circle
+createCircle(popular, lat, long);
+
+park.id = parkInfo.data[j].parkCode; // set ID
+park.bindPopup(parkInfo.data[j].fullName); // create popup
+park.on('click', dataCall); // get data when clicking circle
+}
+}
+}
+}
+
+// draw the circle, change color and size if popular, where popular is a boolean
+function createCircle(popular, lat, long) { 
+    let rad, col; // radius and color
+
+    if(popular) { // change size and color of popular park
+        rad = 40000;
+        col = 'blue';
+    } else {
+        rad = 25000;
+        col = 'red';
+    }
+
+    park = L.circle([lat, long], { // draw the circle
+        color: col,
+        fillColor: '#FFF',
+        fillOpacity: 0.5,
+        radius: rad
+    }).addTo(my_map).on("click", circleClick);
+}
 
 // // add polygons to map
 // var redwood = L.polygon([
-// 	[41.081257, -123.961847],
-// 	[41.088244, -123.940732],
-// 	[41.108036, -123.938844],
-// 	[41.152515, -123.877733],
-// 	[41.227375, -123.962984],
-// 	[41.272418, -123.984614],
-// 	[41.328774, -123.946676],
-// 	[41.464286, -124.036851],
-// 	[41.473161, -124.058308],
-// 	[41.508522, -124.068951],
-// 	[41.522147, -124.044575],
-// 	[41.535769, -124.075646],
-// 	[41.533713, -124.082169],
-// 	[41.312187, -124.087662],
-// 	[41.307545, -124.043889],
-// 	[41.261107, -124.088177],
-// 	[41.207405, -124.061055],
-// 	[41.188805, -124.073071]
-// 	]).addTo(my_map).on("click", circleClick);
+//  [41.081257, -123.961847],
+//  [41.088244, -123.940732],
+//  [41.108036, -123.938844],
+//  [41.152515, -123.877733],
+//  [41.227375, -123.962984],
+//  [41.272418, -123.984614],
+//  [41.328774, -123.946676],
+//  [41.464286, -124.036851],
+//  [41.473161, -124.058308],
+//  [41.508522, -124.068951],
+//  [41.522147, -124.044575],
+//  [41.535769, -124.075646],
+//  [41.533713, -124.082169],
+//  [41.312187, -124.087662],
+//  [41.307545, -124.043889],
+//  [41.261107, -124.088177],
+//  [41.207405, -124.061055],
+//  [41.188805, -124.073071]
+//  ]).addTo(my_map).on("click", circleClick);
 // redwood.id = "redw"
 
 // var sequoia = L.circle([36.4864, -118.5658], {
@@ -108,174 +250,3 @@ let park, popular;
 // channelislands.bindPopup("Channel Islands");
 // deathvalley.bindPopup("Death Valley");
 // yosemite.bindPopup("Yosemite National Park");
-
-// popup functions
-
-// populate map with circles
-$j.ajax({
-    url: NPSurl,
-    method: 'GET',
-}).done((result) => {
-
-    // check if park is included with our buttons
-// for result.length
-    // if parkCode is not in park array
-    // continue
-    // else
-    // getcoords
-
-      getCoords(result); // create circle
-
-
-  }).fail((err) => {
-    throw err;
-});
-
-// display park info on the right side
-function dataCall(e) {
-    $j.ajax({
-        url: NPSurl,
-        method: 'GET',
-    }).done((result) => {
-        displayParkInfo(e.target.id, result);
-    }).fail((err) => {
-        throw err;
-    });
-
-    // show trail select box
-    if (document.getElementById("Gallery").style.display === "none") {
-        document.getElementById("Trails").style.display = "block";
-        document.getElementById("Weather").style.display = "none";
-    } else {
-        document.getElementById("Trails").style.display = "none";
-        document.getElementById("Weather").style.display = "none";
-    }
-
-	// get park info from map
-    var idText = $('#' + e.target.id).text();
-    const reqURL = 'parks/' + idText;
-
-    console.log(reqURL);
-
-    $j.ajax({
-        url: reqURL,
-        type: 'GET',
-        dataType: 'json',
-        success: (data) => {
-
-            $('#intro').html(data.intro);
-
-            // load trail names into select box
-            if ($("#trailSelect").html() == 0 || $("#trailSelect").html() != data.trails) {
-                $("#trailSelect").html(''); // clear select box
-
-                for (var i = 0; i < data.trails.length; i++) {
-                    // append correct trail names
-                    let trail_option = '<option value="' + i + '">' + data.trails[i].name + '</option>';
-                    $("#trailSelect").append(trail_option);
-                }
-                trail_data = data.trails;
-            }
-            $('#pics').html(data.pic);
-        }
-    });
-}
-
-function circleClick(e) {
-    my_map.fitBounds(e.target.getBounds());
-}
-
-function getCoords(parkInfo) {
-    for (let j = 0; j < parkInfo.data.length; j++) {
-
-        // only put circle on map if it is one we want to have
-        if (parkInfo.data[j].parkCode === 'redw' ||
-            parkInfo.data[j].parkCode === 'seki' ||
-            parkInfo.data[j].parkCode === 'jotr' ||
-            parkInfo.data[j].parkCode === 'alca' ||
-            parkInfo.data[j].parkCode === 'deva' ||
-            parkInfo.data[j].parkCode === 'cabr' ||
-            parkInfo.data[j].parkCode === 'yose' ||
-            parkInfo.data[j].parkCode === 'samo' ||
-            parkInfo.data[j].parkCode === 'muwo' ||
-            parkInfo.data[j].parkCode === 'pinn' ||
-            parkInfo.data[j].parkCode === 'camo' ||
-            parkInfo.data[j].parkCode === 'lavo' ||
-            parkInfo.data[j].parkCode === 'moja') {
-
-// get lat and longitude coordinates
-let lat = '';
-let long = '';
-let i = 0;
-
-if (parkInfo.data[j].latLong === "") { // validity check
-    continue;
-}
-// parse out lat and long coordinates
-while (parkInfo.data[j].latLong[i] != ':') {
-    i++;
-}
-
-while (parkInfo.data[j].latLong[i + 1] != ',') {
-    lat = lat + parkInfo.data[j].latLong[i + 1];
-    i++;
-}
-
-while (parkInfo.data[j].latLong[i] != ':') {
-    i++;
-}
-
-for (i; i < parkInfo.data[j].latLong.length - 1; i++) {
-    long = long + parkInfo.data[j].latLong[i + 1];
-}
-
-
-// check if park is popular
-if (parkInfo.data[j].parkCode === 'redw' || 
-    parkInfo.data[j].parkCode === 'seki' ||
-    parkInfo.data[j].parkCode === 'jotr' ||
-    parkInfo.data[j].parkCode === 'deva' ||
-    parkInfo.data[j].parkCode === 'yose') {
-    popular = 1;
-} else {
-  popular = 0;
-}
-
-// create clickable circle
-createCircle(popular, lat, long);
-
-park.id = parkInfo.data[j].parkCode; // set ID
-park.bindPopup(parkInfo.data[j].fullName); // create popup
-park.on('click', dataCall); // get data when clicking circle
-}
-}
-}
-
-function createCircle(popular, lat, long) {
-    let rad, col;
-
-    if(popular) { // change size and color of popular park
-        rad = 40000;
-        col = 'blue';
-    } else {
-        rad = 25000;
-        col = 'red';
-    }
-
-    park = L.circle([lat, long], { 
-        color: col,
-        fillColor: '#FFF',
-        fillOpacity: 0.5,
-        radius: rad
-    }).addTo(my_map).on("click", circleClick);
-}
-
-// redwood.on('click', dataCall);
-// sequoia.on('click', dataCall);
-// joshua.on('click', dataCall);
-// alcatraz.on('click', dataCall);
-// cabrillo.on('click', dataCall);
-// castlemountains.on('click', dataCall);
-// channelislands.on('click', dataCall);
-// deathvalley.on('click', dataCall);
-// yosemite.on('click', dataCall);
