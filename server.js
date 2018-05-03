8890afd3ec00eafd09761fe9437f38f3ccafb3a1
Grace Chen <gpc001@ucsd.edu>
2018-05-02 17:08:38 -0700

    port

--- server.js
+++ server.js
@@ -117,6 +117,10 @@ app.get('/places', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-	console.log('Listening on Port 3000');
+// app.listen(3000, () => {
+// 	console.log('Listening on Port 3000');
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
