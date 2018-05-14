2629ee8f047a5a1d770f80f809d87b8c4e0997c6
Aaron Liu <a3liu@ucsd.edu>
2018-05-14 03:24:12 -0700

    Individual trail lists appear now, but the previous park's trails don't clear when a new park is clicked.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const unirest = require('unirest');
 const bodyParser = require('body-parser');
 
-const parkCoords = require('./backendjs/parkCoords');
 // var tinify = require("tinify"); // image compressor
 // tinify.key = "-ZwwutwwYVLWKCZQnmDcMlSuDpWs5FFP";
 // tinify.proxy = "http://user:pass@192.168.0.1:8080";
@@ -120,44 +119,69 @@ app.get('/places', (req, res) => {
 	const val = tempDatabase[parkToLookUp];
 });
 
-app.post('/trails/:parkid', (req, res) => {
-    console.log(req.body);
-    const trailURLTrue = req.body.parkLocation;
-    /* 
-    for (let i = 0; i < result.places.length; i++) {
-        trail_names[i] = result.places[i].name; // add trail names to array
-    }
+app.post('/trails/:parkid', (request, response) => {
+    console.log(request.body);
+    const trailURL = request.body.parkLocation;
+    let trailList = [];
 
-    // useless line, but can be modified later to give better data
-    const allTrails = Object.keys(result.places[0]);
-    res.send(trail_names); // send array of all trail names in park
-    */
-});
-
-// FIND A WAY TO GENERALIZE THIS URL USING LAT AND LONG coordinates from other API
-// EXECUTE THIS LATER, WHEN THE BUTTON HAS BEEN CLICKED
-let trailURL = "https://trailapi-trailapi.p.mashape.com/?lat=36.4864&lon=-118.5658" +
-"&q[activities_activity_type_name_eq]=hiking&radius=75";
-
-
-// get trails from API through some magic code
-// this is trails in sequoia only for now
-function getTrails(callback) {
 	unirest.get(trailURL)
 	.header("X-Mashape-Key", "df9p8FHPrfmsh9SYeNClLGjG6bOap1kgwbijsn5hQ5dJ9NGLAJ")
 	.header("Accept", "text/plain")
 	.end(function(res) {
 		if (res.error) {
 			//console.log('GET error', res.error);
-			callback(res.error, null);
+			makeTrailList(res.error, null);
 		} else {
-			// console.log('GET response', res.body); // this is the full JSON object
-			callback(null, res.body); // callback returns the JSON from unirest
+			//console.log('GET response', res.body); // this is the full JSON object
+			trailList = makeTrailList(null, res.body); // callback returns the JSON from unirest
+            response.send(trailList);
 		}
 	});
-}
+});
+
+// FIND A WAY TO GENERALIZE THIS URL USING LAT AND LONG coordinates from other API
+// EXECUTE THIS LATER, WHEN THE BUTTON HAS BEEN CLICKED
+/*
+let trailURL = "https://trailapi-trailapi.p.mashape.com/?lat=36.4864&lon=-118.5658" +
+"&q[activities_activity_type_name_eq]=hiking&radius=75";
+*/
+
+/*
+// get trails from API through some magic code
+// this is trails in sequoia only for now
+function getTrails(callback) {
+}*/
 
 // use the function above to get the actual JSON and send it to frontend
+
+function makeTrailList(error, result) {
+
+	let trail_names = []; // array of all trail names in park (sequoia only for now)
+
+	if (error === null) {
+		//console.log(result);
+
+		for (let i = 0; i < result.places.length; i++) {
+			//console.log(result.places[i].name);
+			// console.log("Description: "  + result.places[i].description);
+		}
+
+        for (let i = 0; i < result.places.length; i++) {
+            trail_names[i] = result.places[i].name; // add trail names to array
+        }
+
+        // useless line, but can be modified later to give better data
+        const allTrails = Object.keys(result.places[0]);
+
+        console.log(trail_names);
+        return trail_names; // send array of all trail names in park
+
+	} else {
+		console.log("err");
+	}
+}
+
+/*
 var request = getTrails(function(error, result) {
 	let trail_names = []; // array of all trail names in park (sequoia only for now)
 
@@ -184,12 +208,11 @@ var request = getTrails(function(error, result) {
 			// useless line, but can be modified later to give better data
 			const allTrails = Object.keys(result.places[0]);
 			res.send(trail_names); // send array of all trail names in park
-
 		});
 	} else {
 		console.log("err");
 	}
-});
+});*/
 
 // start port
 app.listen(process.env.PORT || 3000, function(){
